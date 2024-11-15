e94c95ca9dc8a1dd5a67ae973e63e965e1596465
baagii1 <ben@uniqsoft.tech>
2024-11-16 01:30:03 +0700

    feat: replace Counter contract and add Trustme contract with initial setup

--- onchain/src/Trustme.sol
+++ onchain/src/Trustme.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract Counter {
     uint256 public number;
 
