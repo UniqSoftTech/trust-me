bea9b632693992eebe3f3d52761b99419fb5894b
baagii1 <ben@uniqsoft.tech>
2024-11-16 20:06:00 +0700

    feat: update customer data and add new customer entries

--- backend/src/services/customer.service.ts
+++ backend/src/services/customer.service.ts
@@ -12,7 +12,7 @@ const customers = [
     lastname: "Doe",
     email: "doe@gmail.com",
     image: "/image/test.jpg",
-    isEmployee: false,
+    isEmployee: true,
     hourPrice: "20$",
     description: "I am a full stack developer",
   },
@@ -25,7 +25,7 @@ const customers = [
     image: "/image/test.jpg",
     isEmployee: false,
     hourPrice: "30$",
-    description: "Searching a full stack developer",
+    description: "Searching a part-time full stack developer",
   },
   {
     id: 3,
@@ -36,7 +36,51 @@ const customers = [
     image: "/image/test.jpg",
     isEmployee: true,
     hourPrice: "10$",
-    description: "Developer",
+    description: "Frontend Developer",
+  },
+  {
+    id: 4,
+    account: "0x123458",
+    firstname: "Geleg",
+    lastname: "Balsan",
+    email: "Geleg@gmail.com",
+    image: "/image/test.jpg",
+    isEmployee: true,
+    hourPrice: "10$",
+    description: "Junior Developer",
+  },
+  {
+    id: 5,
+    account: "0x123458",
+    firstname: "Grey",
+    lastname: "Gerelt",
+    email: "Grey@gmail.com",
+    image: "/image/test.jpg",
+    isEmployee: true,
+    hourPrice: "20$",
+    description: "Senior Developer",
+  },
+  {
+    id: 6,
+    account: "0x123458",
+    firstname: "Jay",
+    lastname: "Jamka",
+    email: "Jay@gmail.com",
+    image: "/image/test.jpg",
+    isEmployee: false,
+    hourPrice: "30$",
+    description: "Employer for developer",
+  },
+  {
+    id: 7,
+    account: "0x123458",
+    firstname: "Ben",
+    lastname: "Bat-Erdene",
+    email: "Ben@uniqsoft.mn",
+    image: "/image/test.jpg",
+    isEmployee: false,
+    hourPrice: "30$",
+    description: "Searching frontend developer",
   },
 ];
 
@@ -63,9 +107,9 @@ const getCustomersByType = async (isEmployee: boolean) => {
     const filteredValue = customers.find((customer) => customer.isEmployee === isEmployee);
     return filteredValue;
 
-    const contract = new web3.eth.Contract(abi, config.contract_address);
-    const result = await contract.methods.getCustomersByType(isEmployee).call();
-    return result;
+    // const contract = new web3.eth.Contract(abi, config.contract_address);
+    // const result = await contract.methods.getCustomersByType(isEmployee).call();
+    // return result;
   } catch (error: any) {
     return customers;
   }
@@ -75,9 +119,9 @@ const createCustomersByType = async (data: any) => {
   try {
     return customers[0];
 
-    const contract = new web3.eth.Contract(abi, config.contract_address);
-    const result = await contract.methods.createCustomer(data).call();
-    return result;
+    // const contract = new web3.eth.Contract(abi, config.contract_address);
+    // const result = await contract.methods.createCustomer(data).call();
+    // return result;
   } catch (error: any) {
     return customers[0];
   }
@@ -87,9 +131,9 @@ const getCustomerHistory = async (id: any) => {
   try {
     return customers.find((customer) => customer.id == id);
 
-    const contract = new web3.eth.Contract(abi, config.contract_address);
-    const result = await contract.methods.getCustomerById(id).call();
-    return result;
+    // const contract = new web3.eth.Contract(abi, config.contract_address);
+    // const result = await contract.methods.getCustomerById(id).call();
+    // return result;
   } catch (error: any) {
     return customers[0];
   }
@@ -98,9 +142,9 @@ const getCustomerHistory = async (id: any) => {
 const getCustomerByAccount = async (account: any) => {
   try {
     return customers.find((customer) => customer.account == account);
-    const contract = new web3.eth.Contract(abi, config.contract_address);
-    const result = await contract.methods.getCustomerById(account).call();
-    return result;
+    // const contract = new web3.eth.Contract(abi, config.contract_address);
+    // const result = await contract.methods.getCustomerById(account).call();
+    // return result;
   } catch (error: any) {
     return customers[0];
   }
